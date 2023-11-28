165436048c278f8147c2d57e6304752661179b08
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-28 15:19:43 -0500

    Add files via upload

--- components/styles.js
+++ components/styles.js
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import { View, Text, Image } from 'react-native';
+import Constants from 'expo-constants';
+
+const StatusBarHeight = Constants.statusBarHeight;
 
 export const Colors = {
   primary: '#ffffff',
@@ -9,3 +13,31 @@ export const Colors = {
   green: '#10B981',
   red: '#EF4444',
 };
+
+const { primary, secondary, tertiary, darkLight, brand, green, red } = Colors;
+
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  padding-top: ${StatusBarHeight + 10}px;
+  background-color: ${primary};
+`;
+
+export const InnerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+`;
+
+export const PageLogo = styled.View`
+  width: 250px;
+  height: 200px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${brand};
+  padding: 10px;
+`;
