9b38d4bf3c294fb97d327dedbbca5caa06784e75
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-28 15:04:42 -0500

    Add files via upload

---
+++ components/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Colors = {
+  primary: '#ffffff',
+  secondary: '#E5E7EB',
+  tertiary: '#1f2937',
+  darkLight: '#9CA3AF',
+  brand: '#6D28D9',
+  green: '#10B981',
+  red: '#EF4444',
+};
